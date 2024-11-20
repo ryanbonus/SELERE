6ceb9db60ec049deb9a5a330e44930111635e546
Zack Slutzky <zackslutzky@Zachs-MacBook-Pro.local>
2024-11-20 18:45:15 -0500

    extends and retracts before prompting the next message and changed dial to button3 and works the same as mode

--- classes.py
+++ classes.py
@@ -18,7 +18,6 @@ class KneeMotor:
         self.acceleration = acceleration
         print("Extending Knee: Range [{}-{}], Speed {}, Acceleration {}".format(
             rangeOfMotionBottom, rangeOfMotionTop, speed, acceleration))
-        # Simulate extension time
         time.sleep(1)
 
     def retract(self, rangeOfMotionTop, rangeOfMotionBottom, speed, acceleration):
@@ -28,7 +27,6 @@ class KneeMotor:
         self.acceleration = acceleration
         print("Retracting Knee: Range [{}-{}], Speed {}, Acceleration {}".format(
             rangeOfMotionBottom, rangeOfMotionTop, speed, acceleration))
-        # Simulate retraction time
         time.sleep(1)
 
     def assist(self, torque):
@@ -40,10 +38,41 @@ class KneeMotor:
         print("Resisting Knee with Torque:", torque)
 
 
-# Class for Ankle Motor, inheriting from Knee Motor
-class AnkleMotor(KneeMotor):
+# Class for Ankle Motor
+class AnkleMotor:
     def __init__(self):
-        super().__init__()  # Inherit all attributes and methods
+        self.position = 0
+        self.speed = 0
+        self.acceleration = 0
+        self.torque = 0
+        self.rangeOfMotionTop = 100
+        self.rangeOfMotionBottom = 0
+
+    def extend(self, rangeOfMotionTop, rangeOfMotionBottom, speed, acceleration):
+        self.rangeOfMotionTop = rangeOfMotionTop
+        self.rangeOfMotionBottom = rangeOfMotionBottom
+        self.speed = speed
+        self.acceleration = acceleration
+        print("Extending Ankle: Range [{}-{}], Speed {}, Acceleration {}".format(
+            rangeOfMotionBottom, rangeOfMotionTop, speed, acceleration))
+        time.sleep(1)
+
+    def retract(self, rangeOfMotionTop, rangeOfMotionBottom, speed, acceleration):
+        self.rangeOfMotionTop = rangeOfMotionTop
+        self.rangeOfMotionBottom = rangeOfMotionBottom
+        self.speed = speed
+        self.acceleration = acceleration
+        print("Retracting Ankle: Range [{}-{}], Speed {}, Acceleration {}".format(
+            rangeOfMotionBottom, rangeOfMotionTop, speed, acceleration))
+        time.sleep(1)
+
+    def assist(self, torque):
+        self.torque = torque
+        print("Assisting Ankle with Torque:", torque)
+
+    def resist(self, torque):
+        self.torque = torque
+        print("Resisting Ankle with Torque:", torque)
 
 
 # Class for User Interface
@@ -52,7 +81,9 @@ class UserInterface:
         self.modeButton = False
         self.button1 = False
         self.button2 = False
-        self.dialState = 0
+        self.button3 = False
+        self.button3_state = 0
+        self.button3_states = [0, 1, 2, 3]  # Example states for button3
 
     def press_mode_button(self):
         self.modeButton = True
@@ -72,9 +103,13 @@ class UserInterface:
     def release_button2(self):
         self.button2 = False
 
-    def set_dial_state(self, state):
-        self.dialState = state
-        print("Dial State Set to:", state)
+    def press_button3(self):
+        self.button3 = True
+        self.button3_state = (self.button3_state + 1) % len(self.button3_states)
+        print("Button3 State Changed to:", self.button3_state)
+
+    def release_button3(self):
+        self.button3 = False
 
 
 # Exoskeleton Class containing modes and motors
@@ -91,72 +126,37 @@ class Exoskeleton:
         self.currentMode = self.modes[(current_index + 1) % len(self.modes)]
         print("Switched to", self.currentMode)
 
-    def check_conditions(self):
-        # Loop to constantly check and apply conditions
-        while True:
-            # Mode switching with mode button
-            if self.userInterface.modeButton:
-                self.nextMode()
-                self.userInterface.release_mode_button()  # Reset button press
+    def handle_knee_motor(self):
+        self.kneeMotor.extend(100, 0, self.userInterface.button3_state, 5)
+        self.kneeMotor.retract(100, 0, self.userInterface.button3_state, 5)
 
-            # Mode-specific behaviors
-            if self.currentMode == "Mode 1":
-                # Extend and retract knee while button1 is pressed
-                if self.userInterface.button1:
-                    self.kneeMotor.extend(100, 0, self.userInterface.dialState, 5)
-                    self.kneeMotor.retract(100, 0, self.userInterface.dialState, 5) #this needs to happen before the menu pops back up, not after. This is important so that only one command is being handled by the motor at once
-
-                # Extend and retract ankle while button2 is pressed
-                if self.userInterface.button2:
-                    self.ankleMotor.extend(100, 0, self.userInterface.dialState, 5) #this needs to happen before the menu pops back up, not after. This is important so that only one command is being handled by the motor at once
-                    self.ankleMotor.retract(100, 0, self.userInterface.dialState, 5)
-
-            elif self.currentMode == "Mode 2":
-                # Apply assistive torque if button1 or button2 is pressed
-                if self.userInterface.button1:
-                    self.kneeMotor.assist(self.userInterface.dialState)
-                if self.userInterface.button2:
-                    self.ankleMotor.assist(self.userInterface.dialState)
-
-            elif self.currentMode == "Mode 3":
-                # Apply resistive torque if button1 or button2 is pressed
-                if self.userInterface.button1:
-                    self.kneeMotor.resist(self.userInterface.dialState)
-                if self.userInterface.button2:
-                    self.ankleMotor.resist(self.userInterface.dialState)
-
-            # Wait before rechecking to prevent excessive CPU usage
-            time.sleep(0.1)
+    def handle_ankle_motor(self):
+        self.ankleMotor.extend(100, 0, self.userInterface.button3_state, 5)
+        self.ankleMotor.retract(100, 0, self.userInterface.button3_state, 5)
 
 
 # Function to start the Exoskeleton and its loop in a separate thread
 def start_exoskeleton():
     exo = Exoskeleton()
-    # Starting the check_conditions function in a new thread
-    condition_thread = threading.Thread(target=exo.check_conditions)
-    condition_thread.daemon = True
-    condition_thread.start()
 
-    # Simulate user inputs in the command line
     while True:
-        command = input("Enter command (mode, button1, button2, dial, quit): ").strip().lower() #'dial' needs to be switched to the identifier 'button3', and the logic needs to change to be similar to how the mode button works
+        command = input("Enter command (mode, button1, button2, button3, quit): ").strip().lower()
 
         if command == "mode":
             exo.userInterface.press_mode_button()
+            exo.nextMode()
+            exo.userInterface.release_mode_button()
         elif command == "button1":
             exo.userInterface.press_button1()
-            time.sleep(0.2)
+            exo.handle_knee_motor()  # Button1 controls Knee Motor
             exo.userInterface.release_button1()
         elif command == "button2":
             exo.userInterface.press_button2()
-            time.sleep(0.2)
+            exo.handle_ankle_motor()  # Button2 controls Ankle Motor
             exo.userInterface.release_button2()
-        elif command.startswith("dial"):
-            try:
-                _, value = command.split()
-                exo.userInterface.set_dial_state(int(value))
-            except ValueError:
-                print("Invalid dial input. Please enter 'dial' followed by a number.")
+        elif command == "button3":
+            exo.userInterface.press_button3()
+            exo.userInterface.release_button3()
         elif command == "quit":
             print("Exiting...")
             break
